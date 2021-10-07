c00336089f8d9a43d72d43c66c05eb1673725847
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-07 13:19:25 +0300

    add welcome.html and data

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.gallery_of_day,name='galleryToday')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to ~ My Gallery App ~')
+    return render(request,'welcome.html')
+
+def gallery_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
