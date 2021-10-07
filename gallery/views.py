342ae1df7b39e35b4251f2a43b50201d6e54aafe
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-07 12:49:33 +0300

    add my welcome page to gallery

--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+def welcome(request):
+    return HttpResponse('Welcome to ~ My Gallery App ~')
